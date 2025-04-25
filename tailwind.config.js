2bcdc6046ce5b6d031c5b437ec37a0c9028b262a
Muhammad Arslan <147331822+ibn-e-Muhammad@users.noreply.github.com>
2025-04-25 15:47:56 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*"], // or wherever your HTML lives
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
